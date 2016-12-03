293b0ce7cc3137c8ce19ab00c55b979edc0a5f29
citrucel <yv_gr@yahoo.com>
2016-12-03 15:22:38 -0500

    Added gold data

--- eval-synsets.sh
+++ eval-synsets.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python word2vec_optimized.py --use --interactive --train_data=data/text8-synsets.txt  --eval_data=data/questions-answers-synsets.txt --save_path=models/synsets
+python word2vec_optimized.py --use --train_data=data/text8-synsets.txt  --eval_data=data/questions-answers-synsets.txt --save_path=models/synsets
+#python word2vec_optimized.py --use --train_data=data/text8-synsets.txt  --eval_data=data/capital-common-countries-synsets.txt --save_path=models/synsets
