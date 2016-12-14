a571f0c0e95132787ec4682832abe58a4736a48d
citrucel <yv_gr@yahoo.com>
2016-12-14 18:22:14 -0500

    Renamed scripts

---
+++ train-synsets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MINCOUNT=5
+EMBSIZE=200
+EPOCHS=150
+TRAININGDATA=data/2016-12-07-text8-synsets.txt
+EVALDATA=gold-data/capital-common-countries.txt
+MODEL=models/synsets
+
+python word2vec_optimized.py --min_count $MINCOUNT --embedding_size $EMBSIZE --epochs_to_train $EPOCHS --train_data=$TRAININGDATA  --eval_data=$EVALDATA --save_path=$MODEL
