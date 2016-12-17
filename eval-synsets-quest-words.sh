a5bfa6d1f0f2942bf69bbd06f9f2c3a6e8ba4d49
citrucel <yv_gr@yahoo.com>
2016-12-17 09:03:19 -0500

    script changes and results

--- eval-synsets-quest-words.sh
+++ eval-synsets-quest-words.sh
@@ -1,7 +1,7 @@
 
-EPOCH="300"
-MINCUT=5
-EMBSIZE=200
+EPOCH="150"
+MINCUT=1
+EMBSIZE=500
 OUTPUTDIR=gold-data
 TRAINFILE=data/2016-12-07-text8-synsets.txt
 EVALFILE=gold-data/questions-words-synsets.txt
